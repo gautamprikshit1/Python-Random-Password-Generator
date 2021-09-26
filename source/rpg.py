7d0f3399927c246cc64f901ee9ed48454472ae31
Prikshit Gautam <gautamprikshit1@gmail.com>
2021-09-26 10:37:32 +0530

    Take password length(size) from user

--- source/rpg.py
+++ source/rpg.py
@@ -6,7 +6,7 @@ import random
 
 def random_password_generator():
     chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
-    size = 8
+    size = int(input("Please Enter the length of password required: "))
     return ''.join(random.choice(chars) for x in range(size, 20))
 
 def random_password_generator_ico():
